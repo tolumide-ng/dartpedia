a152684d3d0910a99bd23b26898e782cd46dc9a3
tolumide-ng <tolumideshopein@gmail.com>
2026-05-09 00:10:29 +0200

    Adds some classes to extend the Argument abstract

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,60 +1,10 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print("Dartpedia CLI version $version");
-  } else if (arguments.first == 'wikipedia') {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-  }
-}
-
-void printUsage() {
-  print(
-    "The folloiwing commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
-  );
-}
-
-void searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title');
-    final inputFromStdin = stdin.readLineSync();
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting.');
-      return;
-    }
-    articleTitle = inputFromStdin;
-    print("the receiveed article is >>> $articleTitle");
-  } else {
-    articleTitle = arguments.join(' ');
-  }
-
-  print('Looking up articles about "$articleTitle". Please wait.');
-
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent);
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org',
-    '/api/rest_v1/page/summary/$articleTitle',
-  );
-
-  final response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    return response.body;
-  }
-
-  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+void main(List<String> arguments) async {
+  var runner = CommandRunner(); // Create an instance of new CommandRunner
+  await runner.run(arguments);
 }
